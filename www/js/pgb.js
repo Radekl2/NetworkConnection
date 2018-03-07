e09fe52f7188f75a7aed7fe8be188c4a84d02ba0
Radoslaw Smyksy <rsmyksy@gmail.com>
2018-03-07 21:23:18 +0100

    bug fixes

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -7,47 +7,8 @@ function onDeviceReady() {
 }
 
 function showNetworkInfo() {
-	var wifi = cordova.plugins.wifiinfo;
-	wifi.getHostname(function success(info){
-    console.log(info); //
-    /*
-    {
-        hostname: 'ipad-of-user.local',
- 
-        // ConnectionInfo
-        connection: {
-            bssid (string)
-            hidden (boolean): Whether the network is hidden or not
-            ip (string)
-            speed (int): Uplink speed
-            mac (string)
-            rssi (int)
-            ssid (string)
-            frequency (int): WiFi band, Lollipop (API 26+) only
-        },
- 
-        // DhcpInfo
-        dhcp: {
-            dns1 (string)
-            dns2 (string)
-            gateway (string)
-            ip (string)
-            lease (int): Lifespan of DHCP lease
-            netmask (string)
-            server (string)
-        },
- 
-        // list of IPv4 and IPv6 interfaces
-        interfaces: {
-            wlan0: {
-                ipv4Addresses[],
-                ipv6Addresses[]
-            }
-        }
-    }
-    */
-});
+	var info = WifiWizard.isWifiEnabled;
 
-navigator.notification.alert(JSON.stringify(info));
+navigator.notification.alert(info);
 }
 
